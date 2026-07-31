76249688a71971fa4d7ea320fe2960fc7c5fb1df
ABDULAFEEZ12 <tellaafeezadewale@gmail.com>
2026-07-30 23:30:13 -0800

    Update extensions.py

--- app/extensions.py
+++ app/extensions.py
@@ -12,7 +12,7 @@ from flask_talisman import Talisman
 db = SQLAlchemy()
 migrate = Migrate()
 mail = Mail()
-limiter = Limiter(key_func=get_remote_address, default_limits=["200 per day", "50 per hour"])
+limiter = Limiter(key_func=get_remote_address)
 compress = Compress()
 cache = Cache()
 cors = CORS()
